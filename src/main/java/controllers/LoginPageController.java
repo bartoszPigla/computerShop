2281f82d8ed04e219996ddb1850126765e7e42da
bartoszPigla <bartek217a@gmail.com>
2016-02-11 15:18:11 +0100

    first change

---
+++ src/main/java/controllers/LoginPageController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by bartek on 10.02.16.
+ */
+public class LoginPageController {
+}
---
+++ src/main/java/services/RedirectService.java
@@ -0,0 +1,7 @@
+package services;
+
+/**
+ * Created by bartek on 10.02.16.
+ */
+public class RedirectService {
+}
